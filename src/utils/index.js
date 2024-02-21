9b4912a6dded94b541d80da6f20cb1a7a31a8946
Ghaskata <you@example.com>
2024-02-22 01:03:54 +0530

    toggleSubscription,and lliketoggle ,...etc function added

--- src/utils/index.js
+++ src/utils/index.js
@@ -22,11 +22,12 @@ export const authenticate = async (type, payload) => {
         "user",
         JSON.stringify({ ...user, accessToken, refreshToken })
       );
+      window.location.reload();
       return { ...user, accessToken, refreshToken };
     }
   } catch (error) {
     console.log("authenticate errro >>> ", error);
-    toast.error("somthing went's wrong");
+    toast.error("somthing went's wrong", error);
   }
 };
 
@@ -48,13 +49,23 @@ export const fetchVideoAndOwnerDetail = async (videoId) => {
     const videoRes = await axios.get(
       `${process.env.REACT_APP_BE}/videos/${videoId}`,
       {
-        headers:{
-          Authorization:`Bearer ${CurrentUser.accessToken}`
-        }
+        headers: {
+          Authorization: `Bearer ${CurrentUser.accessToken}`,
+        },
       }
     );
     const video = await videoRes.data.data;
-    return video;
+    const isSubscribed = await checkSubscriptionStatus(video.owner._id);
+    // console.log("issubscribed >>> ", isSubscribed);
+    const { totalLikes, likedByMe } = await fetchVideoLike(video._id);
+    // console.log("totalLikes >>> ", totalLikes);
+
+    return {
+      ...video,
+      isSubscribed: isSubscribed,
+      totalLikes: totalLikes,
+      likedByMe: likedByMe,
+    };
   } catch (error) {
     throw new Error(error);
   }
@@ -65,14 +76,160 @@ export const checkSubscriptionStatus = async (userId) => {
     const res = await axios.get(
       `${process.env.REACT_APP_BE}/subscription/${userId}`,
       {
-        headers:{
-          Authorization:`Bearer ${CurrentUser.accessToken}`
-        }
+        headers: {
+          Authorization: `Bearer ${CurrentUser.accessToken}`,
+        },
+      }
+    );
+    const isSubscribed = await res.data.data.isSubscribed;
+    return isSubscribed;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const toggleSubscription = async (userId) => {
+  try {
+    const res = await axios.post(
+      `${process.env.REACT_APP_BE}/subscription/c/${userId}`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${CurrentUser.accessToken}`,
+        },
+      }
+    );
+    const status = res.data.success;
+    toast(res.data.message);
+
+    // return status;
+  } catch (error) {
+    toast.error(error);
+    console.log("erroe >>> ", error);
+    // throw new Error(error);
+  }
+};
+
+export const videoAddViewHistory = async (videoId) => {
+  try {
+    const res = await axios.patch(
+      `${process.env.REACT_APP_BE}/videos/play/${videoId}`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${CurrentUser.accessToken}`,
+        },
+      }
+    );
+    const updatedVideo = await res;
+    console.log("updated video >>>> ", updatedVideo);
+    return updatedVideo;
+  } catch (error) {
+    console.log("erro>>", error);
+    toast.error(error);
+  }
+};
+
+export const fetchVideoLike = async (videoId) => {
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_BE}/likes/v/${videoId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${CurrentUser.accessToken}`,
+        },
       }
     );
     const data = await res.data.data;
+    // console.log("like data ?> ", data);
     return data;
   } catch (error) {
+    toast.error("like errro ", error);
+    throw new Error(error);
+  }
+};
+
+export const toggleLikeToVideo = async (videoId) => {
+  try {
+    const res = await axios.post(
+      `${process.env.REACT_APP_BE}/likes/toggle/v/${videoId}`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${CurrentUser.accessToken}`,
+        },
+      }
+    );
+    console.log(res);
+    // const data = await res.data.data;
+    // toast(res.data.message);
+  } catch (error) {
+    toast.error("toggle like >> ");
+    throw new Error(error);
+  }
+};
+
+export const fetchVideoComments = async (videoId) => {
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_BE}/comments/${videoId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${CurrentUser.accessToken}`,
+        },
+      }
+    );
+    const data = await res.data.data;
+    console.log("comment data ?> ", data);
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const addCommentOnVideo = async (videoId, payload) => {
+  const BACKEND_URL = process.env.REACT_APP_BE;
+  console.log("payload", payload, videoId);
+  try {
+    const res = await axios.post(
+      `${BACKEND_URL}/comments/${videoId}`,
+      payload,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${CurrentUser.accessToken}`,
+        },
+      }
+    );
+
+    console.log("comment add replay >> ", res);
+
+    if (res.data.status === 200) {
+      toast.success("comment added successfully");
+    } else {
+      toast(res.data.data);
+    }
+  } catch (error) {
+    console.log("add comment error", error);
+    toast.error("add comment errro", error);
+  }
+};
+
+export const fetchCommentLike = async (commentId) => {
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_BE}/likes/c/${commentId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${CurrentUser.accessToken}`,
+        },
+      }
+    );
+    const data = await res.data.data;
+    // console.log("like data ?> ", data);
+    return data;
+  } catch (error) {
+    toast.error("like errro ", error);
     throw new Error(error);
   }
 };
